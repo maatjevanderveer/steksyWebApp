002abb9f98f52fef8d496f60cd49cfd357dd21ec
Kirsten de Haas <kirstenlaradehaas@hotmail.com>
2017-03-09 11:52:58 +0100

    models created

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Sequelize = require ('sequelize')
 const bodyParser = require ('body-parser')
 const app = express ()
 const session = require('express-session')
+const db = require(__dirname + '/models/db.js')
+
 
 
 //setting view folder and view engine
@@ -12,6 +14,23 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to ssupport URL-encoded bodies
 	extended: false
 })); 
+
+// // SET UP CONNECTION WITH DATABASE
+// var db = new Sequelize ('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/steksy');
+
+// // TESTING DATABASE CONNECTION
+// db
+// .authenticate()
+// .then(function(err) {
+// 	console.log('Connection has been established successfully.');
+// }, function (err) {
+// 	console.log('Unable to connect to the database:', err);
+// });
+
+
+
+
+
 
 app.use(express.static('static'));
 
